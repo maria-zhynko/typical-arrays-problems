d1b8434732237d2672ee04c98923c2bc90ec3505
maria-zhynko <66383351+maria-zhynko@users.noreply.github.com>
2020-10-03 20:48:13 +0300

    feat: change functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+    return 0;
+  }else{
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+    return 0;
+  }else{
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+    return 0;
+  }else{
+ let summ = 0;
+  for (let i = 1; i < array.length; i++) {
+   summ+=array[i];
+  }
+    let avg=summ/array.length;
+  return avg;}
 }
