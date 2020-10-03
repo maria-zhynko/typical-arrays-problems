dd03b2e476f63a6c05c93868e6cf2b89b7cc4eec
maria-zhynko <66383351+maria-zhynko@users.noreply.github.com>
2020-10-03 20:53:37 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 
 exports.min = function min (array) {
-    if(array === undefined || array.length == 0){
+     if ( !Array.isArray(array) || !array.length){
     return 0;
-  }else{
+  }
   let min = array[0];
   for (let i = 1; i < array.length; i++) {
     if (array[i] < min) {
       min = array[i];
     }
   }
-  return min;}
+  return min;
 }
 
 exports.max = function max (array) {
-    if(array === undefined || array.length == 0){
+     if ( !Array.isArray(array) || !array.length){
     return 0;
-  }else{
+  }
   let max = array[0];
   for (let i = 1; i < array.length; i++) {
     if (array[i] > max) {
       max = array[i];
     }
   }
-  return max;}
+  return max;
 }
 
 exports.avg = function avg (array) {
-    if(array === undefined || array.length == 0){
+     if ( !Array.isArray(array) || !array.length){
     return 0;
-  }else{
+  }
  let summ = 0;
   for (let i = 1; i < array.length; i++) {
    summ+=array[i];
   }
     let avg=summ/array.length;
-  return avg;}
+  return avg;
 }
